966e7520542520aca46db0c9a6e73e79c6e42c1e
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2022-09-28 13:47:18 +0800

    fixed issue where Giscus pops up in homepage - needed to add the condition so it doesn't - added metadata title

--- src/theme/BlogPostItem/Footer/index.js
+++ src/theme/BlogPostItem/Footer/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import Footer from '@theme-original/BlogPostItem/Footer';
+import { useBlogPost } from "@docusaurus/theme-common/internal";
+import Head from "@docusaurus/Head";
 import Giscus from '@giscus/react';
 
 export default function FooterWrapper(props) {
+  const { metadata, isBlogPostPage } = useBlogPost();
+
+  if (!isBlogPostPage) {
+    return <Footer {...props} />
+  }
+
   return (
     <>
+      <Head>
+        <title>{metadata.title}</title>
+      </Head>
       <Footer {...props} />
       <div className='margin-vert--xl'>
         <Giscus 
